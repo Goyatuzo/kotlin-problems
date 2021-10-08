5417fe1ca05693d16ccb5d7cd9c7f74c83102f54
Yuto Otaguro <yotagur@gmail.com>
2021-10-08 04:50:04 +0000

    Two sums (#1)
    * Create tests and source file
    
    * Implement solution
    
    * Thought forEachIndexed couldn't return

---
+++ src/main/kotlin/leetCode/TwoSum.kt
@@ -0,0 +1,16 @@
+package leetCode
+
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val hash = hashMapOf<Int, Int>()
+
+        nums.forEachIndexed { idx, num ->
+            if (hash.containsKey(num)) {
+                return intArrayOf(hash.getOrElse(num) { -1 }, idx)
+            }
+            hash[target - num] = idx
+        }
+
+        return intArrayOf()
+    }
+}
---
+++ src/test/kotlin/leetCode/TwoSumTest.kt
@@ -0,0 +1,24 @@
+package leetCode
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
+
+class TwoSumTest {
+    private val sol = Solution()
+
+    @Test
+    fun `list = (2, 7, 11, 15), target = 9 should result (0, 1)`() {
+        assertContentEquals(intArrayOf(0, 1), sol.twoSum(intArrayOf(2, 7, 11, 15), 9))
+    }
+
+    @Test
+    fun `list = (3, 2, 4), target = 6 should result (1, 2)`() {
+        assertContentEquals(intArrayOf(1, 2), sol.twoSum(intArrayOf(3, 2, 4), 6))
+    }
+
+    @Test
+    fun `list = (3, 3), target = 6 should result (0, 1)`() {
+        assertContentEquals(intArrayOf(0, 1), sol.twoSum(intArrayOf(3, 3), 6))
+    }
+}
