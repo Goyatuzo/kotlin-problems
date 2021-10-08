0a69adf33bb54f25dacdf9f06a9d7a73c5376150
Goyatuzo <yotagur@gmail.com>
2021-10-07 23:45:46 -0500

    Implement solution

--- src/main/kotlin/leetCode/TwoSum.kt
+++ src/main/kotlin/leetCode/TwoSum.kt
@@ -2,6 +2,16 @@ package leetCode
 
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        return nums
+        val hash = hashMapOf<Int, Int>()
+        var match = intArrayOf()
+
+        nums.forEachIndexed { idx, num ->
+            if (hash.containsKey(num)) {
+                match = intArrayOf(hash.getOrElse(num) { -1 }, idx)
+            }
+            hash[target - num] = idx
+        }
+
+        return match
     }
 }
