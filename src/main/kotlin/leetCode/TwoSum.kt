2ab87d339cd1cd49ee00f0347da06bd72cf6596c
Goyatuzo <yotagur@gmail.com>
2021-10-07 23:49:37 -0500

    Thought forEachIndexed couldn't return

--- src/main/kotlin/leetCode/TwoSum.kt
+++ src/main/kotlin/leetCode/TwoSum.kt
@@ -3,15 +3,14 @@ package leetCode
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
         val hash = hashMapOf<Int, Int>()
-        var match = intArrayOf()
 
         nums.forEachIndexed { idx, num ->
             if (hash.containsKey(num)) {
-                match = intArrayOf(hash.getOrElse(num) { -1 }, idx)
+                return intArrayOf(hash.getOrElse(num) { -1 }, idx)
             }
             hash[target - num] = idx
         }
 
-        return match
+        return intArrayOf()
     }
 }
