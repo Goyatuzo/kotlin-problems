da0b3e687105b22892aec0d400d87a421c76f16d
Goyatuzo <yotagur@gmail.com>
2021-10-08 23:46:56 -0500

    Complete question 1

--- src/main/kotlin/AdventOfCode/v2018/Day1/ChronalCalibration.kt
+++ src/main/kotlin/AdventOfCode/v2018/Day1/ChronalCalibration.kt
@@ -1,5 +1,8 @@
 package adventOfCode.v2018.day1
 
+import java.io.File
+
+
 class ChronalCalibration {
     companion object {
         fun calculate(frequencies: List<String>) = frequencies.fold(0) { acc, curr ->
@@ -7,5 +10,15 @@ class ChronalCalibration {
 
             acc + if (curr.first() == '+') value.toInt() else -(value.toInt())
         }
+
+        fun solve() {
+            val text = this::class.java.getResource("/adventOfCode/2018/day1.txt")?.readText(Charsets.UTF_8)
+
+            println(calculate(text!!.lines().filter { it.isNotEmpty() }))
+        }
     }
+}
+
+fun main() {
+    ChronalCalibration.solve()
 }
