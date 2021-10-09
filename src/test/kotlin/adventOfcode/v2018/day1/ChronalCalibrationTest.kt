89987cb36053353914141b58cad394839e9c9394
Goyatuzo <yotagur@gmail.com>
2021-10-08 23:51:48 -0500

    Better naming for tests

--- src/test/kotlin/adventOfcode/v2018/day1/ChronalCalibrationTest.kt
+++ src/test/kotlin/adventOfcode/v2018/day1/ChronalCalibrationTest.kt
@@ -6,22 +6,22 @@ import kotlin.test.assertEquals
 
 class ChronalCalibrationTest {
     @Test
-    fun `1 - (+1, -2, +3, +1) results in 3`() {
+    fun `P1 (+1, -2, +3, +1) results in 3`() {
         assertEquals(3, ChronalCalibration.partOne(listOf("+1", "-2", "+3", "+1")))
     }
 
     @Test
-    fun `1 - (+1, +1, -2) results in 0`() {
+    fun `P1 - (+1, +1, -2) results in 0`() {
         assertEquals(0, ChronalCalibration.partOne(listOf("+1", "+1", "-2")))
     }
 
     @Test
-    fun `1 - (-1, -2, -3) results in -6`() {
+    fun `P1 - (-1, -2, -3) results in -6`() {
         assertEquals(-6, ChronalCalibration.partOne(listOf("-1", "-2", "-3")))
     }
 
     @Test
-    fun `2 - (+1, -1) results in 0`() {
+    fun `P2 - (+1, -1) results in 0`() {
         assertEquals(-6, ChronalCalibration.partOne(listOf("-1", "-2", "-3")))
     }
 }
