abe884ef862e9c33c53e4810754e9efaa79baffa
Goyatuzo <yotagur@gmail.com>
2021-10-10 00:23:07 -0500

    First attempt

--- src/main/kotlin/projectEuler/p20/FactorialDigitSum.kt
+++ src/main/kotlin/projectEuler/p20/FactorialDigitSum.kt
@@ -1,4 +1,16 @@
 package projectEuler.p20
 
+import java.math.BigInteger
+
 class FactorialDigitSum {
+    companion object {
+        fun solve(fact: Int): BigInteger = when (fact) {
+            0, 1 -> BigInteger.valueOf(1)
+            else -> (2..fact)
+                .fold(BigInteger.valueOf(1)) { acc, curr -> acc * BigInteger.valueOf(curr.toLong()) }
+                .toString()
+                .toCharArray()
+                .sumOf { BigInteger(it.toString()) }
+        }
+    }
 }
--- src/test/kotlin/projectEuler/p20/FactorialDigitSumTest.kt
+++ src/test/kotlin/projectEuler/p20/FactorialDigitSumTest.kt
@@ -1,4 +1,12 @@
 package projectEuler.p20
 
+import org.junit.jupiter.api.Test
+import java.math.BigInteger
+import kotlin.test.assertEquals
+
 class FactorialDigitSumTest {
+    @Test
+    fun `10! should be 27`() {
+        assertEquals(BigInteger.valueOf(27), FactorialDigitSum.solve(10))
+    }
 }
