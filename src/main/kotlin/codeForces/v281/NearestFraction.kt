b99202e9137f16ae02c2a4b8f43a270f78142e61
Goyatuzo <yotagur@gmail.com>
2021-10-12 22:39:45 -0500

    All test cases pass

--- src/main/kotlin/codeForces/v281/NearestFraction.kt
+++ src/main/kotlin/codeForces/v281/NearestFraction.kt
@@ -1,9 +1,31 @@
 package codeForces.v281
 
+import java.math.BigDecimal
+import java.math.BigInteger
+import java.math.MathContext
+
 class NearestFraction {
     companion object {
         fun solve(x: Int, y: Int, n: Int): String {
-            return ""
+            var answer = ""
+            val precision = MathContext(20)
+            val target = BigDecimal(x).divide(BigDecimal(y), precision)
+
+            var closest = BigDecimal(Int.MAX_VALUE)
+
+            (1..n).forEach { denominator ->
+                var numerator = 1
+                do {
+                    val current = BigDecimal(numerator++).divide(BigDecimal(denominator), precision)
+
+                    if ((closest - target).abs() > (current - target).abs()) {
+                        closest = current
+                        answer = "${numerator - 1}/${denominator}"
+                    }
+                } while (current <= target)
+            }
+
+            return answer
         }
     }
 }
--- src/test/kotlin/codeForces/v281/NearestFractionTest.kt
+++ src/test/kotlin/codeForces/v281/NearestFractionTest.kt
@@ -11,6 +11,6 @@ class NearestFractionTest {
 
     @Test
     fun `7, 2, 4 results in 7 over 2`() {
-        assertEquals("7/5", NearestFraction.solve(7, 2, 4))
+        assertEquals("7/2", NearestFraction.solve(7, 2, 4))
     }
 }
