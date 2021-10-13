181aebd9301487858ea0eefe707e5d5982608db3
Goyatuzo <yotagur@gmail.com>
2021-10-12 22:17:50 -0500

    Create unit tests

--- src/main/kotlin/codeForces/v281/NearestFraction.kt
+++ src/main/kotlin/codeForces/v281/NearestFraction.kt
@@ -1,4 +1,9 @@
 package codeForces.v281
 
 class NearestFraction {
+    companion object {
+        fun solve(x: Int, y: Int, n: Int): String {
+            return ""
+        }
+    }
 }
--- src/test/kotlin/codeForces/v281/NearestFractionTest.kt
+++ src/test/kotlin/codeForces/v281/NearestFractionTest.kt
@@ -1,4 +1,16 @@
 package codeForces.v281
 
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
 class NearestFractionTest {
+    @Test
+    fun `3, 7, 6 results in 2 over 5`() {
+        assertEquals("2/5", NearestFraction.solve(3, 7, 6))
+    }
+
+    @Test
+    fun `7, 2, 4 results in 7 over 2`() {
+        assertEquals("7/5", NearestFraction.solve(7, 2, 4))
+    }
 }
