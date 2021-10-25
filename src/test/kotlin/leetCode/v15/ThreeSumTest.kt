39ae95d8284092499fd3c3d02a7bcbbeeb46738c
Goyatuzo <yotagur@gmail.com>
2021-10-24 23:18:29 -0500

    Better unit test for list equality

--- src/test/kotlin/leetCode/v15/ThreeSumTest.kt
+++ src/test/kotlin/leetCode/v15/ThreeSumTest.kt
@@ -2,13 +2,14 @@ package leetCode.v15
 
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class ThreeSumTest {
     private val sol = Solution()
 
     @Test
     fun `(-1, 0, 1, 2, -1, -4) should output ((-1, -1, 2), (-1, 0, 1))`() {
-        assertEquals(listOf(listOf(-1, 0, 1), listOf(-1, -1, 2)), sol.threeSum(intArrayOf(-1, 0, 1, 2, -1, -4)))
+        assertTrue(listOf(listOf(-1, 0, 1), listOf(-1, -1, 2)).containsAll(sol.threeSum(intArrayOf(-1, 0, 1, 2, -1, -4))))
     }
 
     @Test
