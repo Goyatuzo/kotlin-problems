2e017a6e5e8ffa8d1a5f3b14c3a240542e11a74a
Goyatuzo <yotagur@gmail.com>
2021-10-24 23:56:53 -0500

    Update unit tests

--- src/test/kotlin/leetCode/v15/ThreeSumTest.kt
+++ src/test/kotlin/leetCode/v15/ThreeSumTest.kt
@@ -1,24 +1,26 @@
 package leetCode.v15
 
 import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertTrue
+import com.google.common.truth.Truth.assertThat
+import com.google.common.truth.Truth.assertWithMessage
+
 
 class ThreeSumTest {
     private val sol = Solution()
 
     @Test
     fun `(-1, 0, 1, 2, -1, -4) should output ((-1, -1, 2), (-1, 0, 1))`() {
-        assertTrue(listOf(listOf(-1, 0, 1), listOf(-1, -1, 2)).containsAll(sol.threeSum(intArrayOf(-1, 0, 1, 2, -1, -4))))
+        val t = sol.threeSum(intArrayOf(-1, 0, 1, 2, -1, -4))
+        assertThat(t).containsExactly(listOf(listOf(-1, 0, 1), listOf(-1, -1, 2)))
     }
 
     @Test
     fun `() should output ()`() {
-        assertEquals(emptyList(), sol.threeSum(intArrayOf()))
+        assertThat(sol.threeSum(intArrayOf())).isEmpty()
     }
 
     @Test
     fun `(0) should output ()`() {
-        assertEquals(emptyList(), sol.threeSum(intArrayOf(0)))
+        assertThat(sol.threeSum(intArrayOf(0))).isEmpty()
     }
 }
