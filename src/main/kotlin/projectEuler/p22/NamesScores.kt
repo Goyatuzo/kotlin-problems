01a50e225961fab77dc75b5f82b7790d962f49f1
Goyatuzo <yotagur@gmail.com>
2021-11-04 22:48:12 -0500

    Attempt 1

--- src/main/kotlin/projectEuler/p22/NamesScores.kt
+++ src/main/kotlin/projectEuler/p22/NamesScores.kt
@@ -1,10 +1,22 @@
 package projectEuler.p22
 
+import adventOfCode.v2018.day1.ChronalCalibration
 import java.math.BigInteger
 
 class NamesScores {
     companion object {
         fun solveForOne(i: Int, name: String): BigInteger =
             (name.lowercase().fold(0) { acc, curr -> curr.code - ('a'.code - 1) + acc } * i).toBigInteger()
+
+        fun solve(): BigInteger {
+            val text = this::class.java.getResource("/projectEuler/names.txt")?.readText(Charsets.UTF_8)
+            val names = text?.replace("\"","")?.split(',') ?: emptyList()
+
+            return names.foldIndexed(BigInteger.ZERO) { idx, acc, curr -> acc + solveForOne(idx, curr) }
+        }
     }
+}
+
+fun main() {
+    println(NamesScores.solve())
 }
