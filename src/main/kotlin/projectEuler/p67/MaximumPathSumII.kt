d85eeef54147efaed16552f8db5b76271175520e
Yuto Otaguro <yotagur@gmail.com>
2021-11-05 05:17:05 +0000

    Euler 67 (#12)
    * Outline to solve the problem
    
    * Solve problem

---
+++ src/main/kotlin/projectEuler/p67/MaximumPathSumII.kt
@@ -0,0 +1,20 @@
+package projectEuler.p67
+
+import projectEuler.p18.MaximumPathSumI
+import projectEuler.p22.NamesScores
+import java.math.BigInteger
+
+class MaximumPathSumII {
+    companion object {
+        fun solve(): Int? {
+            val text = this::class.java.getResource("/projectEuler/MaximumPathSumII.txt")?.readText(Charsets.UTF_8)
+            val triangle = text?.split('\n')?.map { it.trim().split(' ').map { it.toInt() } } ?: emptyList()
+
+            return MaximumPathSumI.triangle(triangle)
+        }
+    }
+}
+
+fun main() {
+    println("Maximum Path Sum II: ${MaximumPathSumII.solve()}")
+}
