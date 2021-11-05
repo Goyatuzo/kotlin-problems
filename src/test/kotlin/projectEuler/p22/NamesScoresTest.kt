ca11f4d2910d338a3665af5a5b87c8d471d7cd69
Goyatuzo <yotagur@gmail.com>
2021-11-04 22:40:05 -0500

    Test for row

--- src/test/kotlin/projectEuler/p22/NamesScoresTest.kt
+++ src/test/kotlin/projectEuler/p22/NamesScoresTest.kt
@@ -7,7 +7,12 @@ import kotlin.test.assertEquals
 
 class NamesScoresTest {
     @Test
-    fun `COLIN should be 53`() {
+    fun `COLIN on row 1 should be 53`() {
         assertEquals(53.toBigInteger(), NamesScores.solveForOne(1, "COLIN"))
+    }
+
+    @Test
+    fun `COLIN on row 938 should be 49714`() {
+        assertEquals(49714.toBigInteger(), NamesScores.solveForOne(938, "COLIN"))
     }
 }
