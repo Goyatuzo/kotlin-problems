9705f4de2ab971c8061a5b31fd2da0bad9f7e0c0
Goyatuzo <yotagur@gmail.com>
2021-11-04 21:54:40 -0500

    Have an implementation

--- src/main/kotlin/leetCode/v15/ThreeSum.kt
+++ src/main/kotlin/leetCode/v15/ThreeSum.kt
@@ -1,16 +1,17 @@
 package leetCode.v15
 
+// TODO: This is not the most efficient
 class Solution {
     fun threeSum(nums: IntArray): List<List<Int>> = when (nums.size) {
         0, 1, 2 -> emptyList()
-        else -> nums.foldIndexed<List<List<Int>>>(emptyList()) { i, acc, target ->
+        else -> nums.sorted().foldIndexed<Int, List<List<Int>>>(emptyList()) { i, acc, target ->
             val map = mutableMapOf<Int, Int>()
             var currentAccumulation = listOf<List<Int>>()
 
             (i+1 until nums.size).forEach { c ->
                 val curr = nums[c]
                 if (map.containsKey(-curr)) {
-                    currentAccumulation = currentAccumulation + listOf(listOf(target, map.getValue(-curr), curr))
+                    currentAccumulation = currentAccumulation + listOf(listOf(target, curr, map.getValue(-curr)))
                 }
                 map[target + curr] = curr
             }
@@ -21,7 +22,6 @@ class Solution {
                 acc
             }
         }
-            .map { it.sorted() }
             .toSet()
             .toList()
     }
--- src/test/kotlin/leetCode/v15/ThreeSumTest.kt
+++ src/test/kotlin/leetCode/v15/ThreeSumTest.kt
@@ -2,16 +2,13 @@ package leetCode.v15
 
 import org.junit.jupiter.api.Test
 import com.google.common.truth.Truth.assertThat
-import com.google.common.truth.Truth.assertWithMessage
-
 
 class ThreeSumTest {
     private val sol = Solution()
 
     @Test
     fun `(-1, 0, 1, 2, -1, -4) should output ((-1, -1, 2), (-1, 0, 1))`() {
-        val t = sol.threeSum(intArrayOf(-1, 0, 1, 2, -1, -4))
-        assertThat(t).contains(listOf(listOf(-1, 0, 1), listOf(-1, -1, 2)))
+        assertThat(sol.threeSum(intArrayOf(-1, 0, 1, 2, -1, -4))).contains(listOf(listOf(-1, 0, 1), listOf(-1, -1, 2)))
     }
 
     @Test
