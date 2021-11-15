db3af97131b1411b2b6b53548032e98d66acd7f8
Goyatuzo <yotagur@gmail.com>
2021-11-14 22:11:16 -0600

    update test and try a more efficient algo

--- src/test/kotlin/projectEuler/p25/EulerFibonacciNumberTest.kt
+++ src/test/kotlin/projectEuler/p25/EulerFibonacciNumberTest.kt
@@ -8,4 +8,9 @@ class EulerFibonacciNumberTest {
     fun `12 should be first number with three digits`() {
         assertThat(EulerFibonacciNumber.solve(3)).isEqualTo(12)
     }
+
+    @Test
+    fun `4782 should be the first number with 1000 digits`() {
+        assertThat(EulerFibonacciNumber.solve(1000)).isEqualTo(4782)
+    }
 }
