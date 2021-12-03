f50c4686b39a97fd4758551087fa6d6aa2ba55c4
Goyatuzo <yotagur@gmail.com>
2021-12-02 21:23:38 -0600

    Solve part 1

--- src/main/kotlin/AdventOfCode/v2021/day1/SonarSweep.kt
+++ src/main/kotlin/AdventOfCode/v2021/day1/SonarSweep.kt
@@ -5,5 +5,15 @@ class SonarSweep {
         fun partOne(numbers: List<Int>): Int = numbers.foldIndexed(0) { idx, acc, curr ->
             if (idx > 0 && numbers[idx-1] < curr) acc + 1 else acc
         }
+
+        fun solve() {
+            val text = this::class.java.getResource("/adventOfCode/2021/day1.txt")?.readText(Charsets.UTF_8)
+
+            println(partOne(text!!.lines().filter { it.isNotEmpty() }.map { it.toInt() }))
+        }
     }
+}
+
+fun main() {
+    SonarSweep.solve()
 }
