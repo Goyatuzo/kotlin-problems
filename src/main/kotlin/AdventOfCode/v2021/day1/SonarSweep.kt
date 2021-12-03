3c86184585c55aeab24fd0ce9f6653e19c312685
Yuto Otaguro <yotagur@gmail.com>
2021-12-03 03:38:13 +0000

    Aoc 1 (#14)
    * Create implementation
    
    * Create tests
    
    * Clean imports
    
    * Add the file
    
    * Solve part 1
    
    * Write test for part 2
    
    * Complete part 2

---
+++ src/main/kotlin/AdventOfCode/v2021/day1/SonarSweep.kt
@@ -0,0 +1,40 @@
+package adventOfCode.v2021.day1
+
+class SonarSweep {
+    companion object {
+        fun partOne(numbers: List<Int>): Int = numbers.foldIndexed(0) { idx, acc, curr ->
+            if (idx > 0 && numbers[idx-1] < curr) acc + 1 else acc
+        }
+
+        fun partTwo(numbers: List<Int>): Int {
+            if (numbers.count() < 3) return -1
+
+            var total = 0
+            val previous = numbers.subList(0, 3).toMutableList()
+
+            for (currVal in numbers.listIterator(3)) {
+                val previousSum = previous.sum()
+                previous[0] = previous[1]
+                previous[1] = previous[2]
+                previous[2] = currVal
+                val currentSum = previous.sum()
+
+                if (currentSum > previousSum) total++
+            }
+
+            return total
+        }
+
+        fun solve() {
+            val text = this::class.java.getResource("/adventOfCode/2021/day1.txt")?.readText(Charsets.UTF_8)
+
+            println(partOne(text!!.lines().filter { it.isNotEmpty() }.map { it.toInt() }))
+            println(partTwo(text.lines().filter { it.isNotEmpty() }.map { it.toInt() }))
+
+        }
+    }
+}
+
+fun main() {
+    SonarSweep.solve()
+}
---
+++ src/test/kotlin/adventOfcode/v2021/day1/SonarSweepTest.kt
@@ -0,0 +1,17 @@
+package adventOfcode.v2021.day1
+
+import adventOfCode.v2021.day1.SonarSweep
+import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.Test
+
+class SonarSweepTest {
+    @Test
+    fun `Test given in part 1`() {
+        assertThat(SonarSweep.partOne(listOf(199, 200, 208, 210, 200, 207, 240, 269, 260, 263))).isEqualTo(7)
+    }
+
+    @Test
+    fun `Test given in part 2`() {
+        assertThat(SonarSweep.partTwo(listOf(199, 200, 208, 210, 200, 207, 240, 269, 260, 263))).isEqualTo(5)
+    }
+}
