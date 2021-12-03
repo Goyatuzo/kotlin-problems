dfeefeddba09a670340bde52fdecb1be1fc8759e
Goyatuzo <yotagur@gmail.com>
2021-12-02 21:17:08 -0600

    Create tests

--- src/main/kotlin/AdventOfCode/v2021/day1/SonarSweep.kt
+++ src/main/kotlin/AdventOfCode/v2021/day1/SonarSweep.kt
@@ -2,7 +2,7 @@ package adventOfCode.v2021.day1
 
 class SonarSweep {
     companion object {
-        fun solve(numbers: List<Int>): Int = numbers.foldIndexed(0) { idx, acc, curr ->
+        fun partOne(numbers: List<Int>): Int = numbers.foldIndexed(0) { idx, acc, curr ->
             if (idx > 0 && numbers[idx-1] < curr) acc + 1 else acc
         }
     }
---
+++ src/test/kotlin/adventOfcode/v2021/day1/SonarSweepTest.kt
@@ -0,0 +1,13 @@
+package adventOfcode.v2021.day1
+
+import adventOfCode.v2021.day1.SonarSweep
+import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class SonarSweepTest {
+    @Test
+    fun `P1 (+1, -2, +3, +1) results in 3`() {
+        assertThat(SonarSweep.partOne(listOf(199, 200, 208, 210, 200, 207, 240, 269, 260, 263))).isEqualTo(7)
+    }
+}
