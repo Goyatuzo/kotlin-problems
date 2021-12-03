d3136071f5d516414e4d32851b9013db4b92628b
Goyatuzo <yotagur@gmail.com>
2021-12-02 22:00:56 -0600

    Complete part 1

--- src/main/kotlin/AdventOfCode/v2021/day2/Dive.kt
+++ src/main/kotlin/AdventOfCode/v2021/day2/Dive.kt
@@ -1,5 +1,8 @@
 package adventOfCode.v2021.day2
 
+import java.util.*
+
+
 enum class Direction {
     FORWARD,
     UP,
@@ -22,5 +25,20 @@ class Dive {
 
             return x * y
         }
+
+        fun solve() {
+            val text = this::class.java.getResource("/adventOfCode/2021/day2.txt")?.readText(Charsets.UTF_8)
+
+            val movements = text!!.lines().filter { it.isNotEmpty() }.map {
+                val comps = it.split(' ')
+                Pair(Direction.valueOf(comps[0].uppercase()), comps[1].toInt())
+            }
+
+            println(partOne(movements))
+        }
     }
+}
+
+fun main() {
+    Dive.solve()
 }
