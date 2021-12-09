e88ed623b0cd7cdc2c07b51db6ce02be7329999b
Goyatuzo <yotagur@gmail.com>
2021-12-08 22:37:04 -0600

    Suboptimal solution

--- src/main/kotlin/AdventOfCode/v2021/day3/BinaryDiagnostic.kt
+++ src/main/kotlin/AdventOfCode/v2021/day3/BinaryDiagnostic.kt
@@ -16,12 +16,61 @@ class BinaryDiagnostic {
             return Integer.parseUnsignedInt(toBinary, 2).toUInt() * Integer.parseUnsignedInt(toInverse, 2).toUInt()
         }
 
+        fun oxygenGenerator(binaries: List<String>): UInt {
+            var zeros = binaries
+            var ones = binaries
+            var idx = 0
+
+            while (zeros.size > 1) {
+                ones = ones.filter { it[idx] == '1' }
+                zeros = zeros.filter { it[idx] == '0' }
+
+                if (ones.count() < zeros.count()) {
+                    ones = zeros
+                } else {
+                    zeros = ones
+                }
+
+                idx++
+            }
+
+            val binary = if (zeros.isNotEmpty()) zeros.first() else ones.first()
+
+            return Integer.parseUnsignedInt(binary, 2).toUInt()
+        }
+
+        fun scrubberRating(binaries: List<String>): UInt {
+            var zeros = binaries
+            var ones = binaries
+            var idx = 0
+
+            while (zeros.size > 1) {
+                ones = ones.filter { it[idx] == '1' }
+                zeros = zeros.filter { it[idx] == '0' }
+
+                if (ones.count() >= zeros.count()) {
+                    ones = zeros
+                } else {
+                    zeros = ones
+                }
+
+                idx++
+            }
+
+            val binary = if (zeros.isNotEmpty()) zeros.first() else ones.first()
+
+            return Integer.parseUnsignedInt(binary, 2).toUInt()
+        }
+
+        fun partTwo(binaries: List<String>) = oxygenGenerator(binaries) * scrubberRating(binaries)
+
         fun solve() {
             val text = this::class.java.getResource("/adventOfCode/2021/day3.txt")?.readText(Charsets.UTF_8)
 
             val binaries = text!!.lines().filter { it.isNotEmpty() }
 
             println(partOne(binaries))
+            println(partTwo(binaries))
         }
     }
 }
--- src/test/kotlin/adventOfcode/v2021/day3/BinaryDiagnosticTest.kt
+++ src/test/kotlin/adventOfcode/v2021/day3/BinaryDiagnosticTest.kt
@@ -22,4 +22,58 @@ class BinaryDiagnosticTest {
             "01010",
         ))).isEqualTo(198.toUInt())
     }
+
+    @Test
+    fun `Test oxygen generator 2`() {
+        assertThat(BinaryDiagnostic.oxygenGenerator(listOf(
+            "00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010",
+        ))).isEqualTo(23.toUInt())
+    }
+
+    @Test
+    fun `Test scrubber 2`() {
+        assertThat(BinaryDiagnostic.scrubberRating(listOf(
+            "00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010",
+        ))).isEqualTo(10.toUInt())
+    }
+
+    @Test
+    fun `Test given 2`() {
+        assertThat(BinaryDiagnostic.partTwo(listOf(
+            "00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010",
+        ))).isEqualTo(230.toUInt())
+    }
 }
