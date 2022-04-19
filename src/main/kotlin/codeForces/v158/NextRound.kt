b4f34e0f508e091f33a54557bf8521f4f4bc3e8d
Goyatuzo <yotagur@gmail.com>
2022-04-18 23:41:40 -0500

    Naive implementation

---
+++ src/main/kotlin/codeForces/v158/NextRound.kt
@@ -0,0 +1,23 @@
+package codeForces.v158
+
+class NextRound {
+    companion object {
+        fun solve(k: Int, scores: List<Int>): Int = scores.foldIndexed(0) { idx, acc, curr ->
+            if (curr == 0) {
+                acc
+            } else {
+                if (idx > k && scores[k] == curr) {
+                    acc + 1
+                } else if (idx <= k) {
+                    acc + 1
+                } else {
+                    acc
+                }
+            }
+        }
+    }
+}
+
+fun main() {
+
+}
---
+++ src/test/kotlin/codeForces/v158/NextRoundTest.kt
@@ -0,0 +1,21 @@
+package codeForces.v158
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class NextRoundTest {
+    @Test
+    fun `k = 2, 0 0 0 0 is 0`() {
+        assertEquals(0, NextRound.solve(2, listOf(0, 0, 0, 0)))
+    }
+
+    @Test
+    fun `k = 5, 10 9 8 7 7 7 5 5 is 6`() {
+        assertEquals(6, NextRound.solve(5, listOf(10, 9, 8, 7, 7, 7, 5, 5)))
+    }
+
+    @Test
+    fun `k = 5, 10 9 8 0 0 0 0 0 is 3`() {
+        assertEquals(3, NextRound.solve(5, listOf(10, 9, 8, 7, 7, 7, 5, 5)))
+    }
+}
