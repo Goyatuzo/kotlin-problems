dd7770e0d6d989db75ae2cbc248acc6b4b78b87b
Goyatuzo <yotagur@gmail.com>
2022-04-23 01:56:25 -0500

    One more spotless apply

--- src/main/kotlin/codeForces/v281/WordCapitalization.kt
+++ src/main/kotlin/codeForces/v281/WordCapitalization.kt
@@ -5,7 +5,9 @@ import java.util.*
 class WordCapitalization {
     companion object {
         fun solve(input: String): String {
-            return input.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
+            return input.replaceFirstChar {
+                if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString()
+            }
         }
     }
 }
