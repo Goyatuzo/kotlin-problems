acf3012c7dc6fae696945f70b0e1b77e84c978ce
Yuto Otaguro <yotagur@gmail.com>
2022-05-01 06:54:59 +0000

    Cf 231 a (#22)

---
+++ src/main/kotlin/codeForces/v231/Team.kt
@@ -0,0 +1,28 @@
+package codeForces.v231
+
+class Team {
+    companion object {
+        fun solve(thoughts: List<List<Int>>) =
+            thoughts.fold(0) { acc, curr ->
+                when (curr.sum()) {
+                    0, 1 -> acc
+                    else -> acc + 1
+                }
+            }
+    }
+}
+
+fun main() {
+    readLine()
+    val opinions = mutableListOf<List<Int>>()
+
+    do {
+        val line = readLine()
+
+        if (line != null) {
+            opinions.add(line.split(" ").map { it.toInt() })
+        }
+    } while (line != null)
+
+    println(Team.solve(opinions))
+}
